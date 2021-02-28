3dbed975e2c58eec92664297c9fb6768e0a9f1d0
Dennis Peters <dennis95peters@gmail.com>
2021-02-28 18:03:40 +0300

    This commit makes animations on the WHAT WE DO section of the page

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
-$(document).ready (() => {
-    let myForm = $(myForm)
-})
 
-function validate (event){
-    event.preventDefault ();
-    let name = $('#name').val();
-    let email = $('#email').val();
-    let message = $('#message').val();
-   // let output = `${name}; ${email} - ${message}`
-}
+$(document).ready(function() {
 
-if (name) == '' || email == '' || message == '') {
-    name input addclass ('error')
-    alert ('Error! All fields required')
-    return false;
-}
-else {
-    let response = `Hey ${name}. Your message has been received!`
-    alert (response)
-}
+    let cardTotalNumber =(
+        $(".card").toArray().length
+      );
+  
+    for(let cardNumber = 1; cardNumber<=cardTotalNumber;cardNumber++ ){
+        $(`.card${cardNumber}`).click(function() {
+            $(`.card-show${cardNumber}`).toggle(400);
+            $(`.card-hidden${cardNumber}`).toggle(400);
+          });
+      }
+    
+  
+      let overlayTotalNumber =(
+        $(".overlay").toArray().length
+      );
+  
+      for(let overlayNumber = 1; overlayNumber<=overlayTotalNumber;overlayNumber++){
+          $(`#overlay${overlayNumber}`).mouseover(function() {
+          $(`#overlay${overlayNumber} > img`).addClass("overlay-effect");
+          $(`#work-overlay${overlayNumber}`).toggle();
+        }).mouseout(function(){
+          $(`#overlay${overlayNumber} > img`).removeClass("overlay-effect");
+          $(`#work-overlay${overlayNumber}`).toggle();
+        });
+      }
+    
+    
+    
+    
+    });
+    
